5a2dd9a20e2b87eebc9bbc7220b5a70e6b30b039
Jasper Tey <jasper.tey@gmail.com>
2021-07-03 12:17:19 -0400

    Helper for working with stubs.

---
+++ src/Helpers/Stub.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Vio\Pinball\Helpers;
+
+class Stub {
+
+    public static function interpolate($stub, $values)
+    {
+        foreach ($values as $key => $value) {
+            $searches = [
+                "{{{$key}}}",
+                "{{ {$key} }}"
+            ];
+
+            foreach ($searches as $search) {
+                $stub = str_replace($search, $value, $stub);
+            }
+        }
+
+        return $stub;
+    }
+    
+}
