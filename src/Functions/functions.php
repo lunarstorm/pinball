dce5ca9d303b40bbfcb4b162634e598587590077
Jasper Tey <jasper.tey@gmail.com>
2021-07-04 09:17:22 -0400

    Add custom dump functions that were utilized pre-laravel.

---
+++ src/Functions/functions.php
@@ -0,0 +1,57 @@
+<?php
+
+function print_pre($var, $return = null)
+{
+    if (app()->runningInConsole()) {
+        return print_r($var, $return);
+    }
+
+    $s = "<pre>";
+    $s .= print_r($var, true);
+    $s .= "</pre>";
+
+    if ($return == true) {
+        return $s;
+    }
+
+    echo $s;
+}
+
+function pre_dump($var)
+{
+    if (app()->runningInConsole()) {
+        var_dump($var);
+        return;
+    }
+
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+}
+
+if (!function_exists('pp')) {
+    function pp(...$args)
+    {
+        foreach ($args as $arg) {
+            print_pre($arg);
+        }
+    }
+}
+
+if (!function_exists('pd')) {
+    function pd(...$args)
+    {
+        pp(...$args);
+        die();
+    }
+}
+
+if (!function_exists('vd')) {
+    function vd(...$args)
+    {
+        foreach ($args as $arg) {
+            pre_dump($arg);
+        }
+        die();
+    }
+}
