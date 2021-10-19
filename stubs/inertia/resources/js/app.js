fc04154cf44598986059cd60ae1ffcb9a317f616
Jasper Tey <jasper.tey@gmail.com>
2021-10-18 22:11:12 -0400

    Update stub for latest inertia compatibility.

--- stubs/inertia/resources/js/app.js
+++ stubs/inertia/resources/js/app.js
@@ -1,27 +1,29 @@
 require('./bootstrap');
 require('bootstrap');
 require("./theme/looper/javascript/theme");
-import {createApp, defineAsyncComponent, h} from 'vue';
-import {createInertiaApp} from '@inertiajs/inertia-vue3';
-import {InertiaProgress} from '@inertiajs/progress';
+import { createApp, defineAsyncComponent, h } from 'vue';
+import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 import VioPlugin from "vio/plugin/VioPlugin";
 
 const LayoutDefault = defineAsyncComponent(() => import("@/Layouts/LayoutDefault"));
 
 createInertiaApp({
-	resolve: name => import(`./Pages/${name}`)
-			.then(({default: page}) => {
-				if (page.layout === undefined) {
-					page.layout = LayoutDefault
-				}
-				return page
-			}),
-	setup({el, app, props, plugin}) {
-		createApp({render: () => h(app, props)})
-				.use(plugin)
-				.use(VioPlugin)
-				.mount(el)
+    resolve: name => import(`./Pages/${name}`)
+        .then(({ default: page }) => {
+            if (page.layout === undefined) {
+                page.layout = LayoutDefault
+            }
+            return page
+        }),
+    setup({ el, app, props, plugin }) {
+        createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(VioPlugin)
+            .component('InertiaHead', Head)
+            .component('InertiaLink', Link)
+            .mount(el)
 
-		InertiaProgress.init();
-	},
+        InertiaProgress.init();
+    },
 });
