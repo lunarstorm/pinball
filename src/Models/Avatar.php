f0e1fe80457e55c0d407d934a1e7b88f161b9a98
Jasper Tey <jasper.tey@gmail.com>
2021-11-27 18:00:31 -0500

    Avatar model stub, WIP.

---
+++ src/Models/Avatar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vio\Pinball\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends BaseModel
+{
+    use HasTimestamps;
+
+}
