9d4f9c1834ecba6bb5b7f2fba3ca06dbba1f4214
Jasper Tey <jasper.tey@gmail.com>
2021-11-27 18:00:20 -0500

    Beginnings of pattern abstractions from past projects.

---
+++ src/Patterns/ActivityFeed/Models/FeedStory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pinball\Patterns\ActivityFeed\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Vio\Pinball\Models\BaseModel;
+
+class FeedStory extends BaseModel
+{
+    use HasTimestamps;
+
+    
+}
