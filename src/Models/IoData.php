2ca5920c227e18d7196dd679cb4e8f54d34efc42
Jasper Tey <jasper.tey@gmail.com>
2022-04-23 01:15:29 -0400

    IoData model update for legacy usage.

--- src/Models/IoData.php
+++ src/Models/IoData.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use io\data\EntityKeyValueModel;
+use Vio\Pinball\Legacy\io\models\EntityKeyValueModel as ModelsEntityKeyValueModel;
 
-class IoData extends BaseModel
+class IoData extends ModelsEntityKeyValueModel
 {
     use HasFactory, HasTimestamps;
 
