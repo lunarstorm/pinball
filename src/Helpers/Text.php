3f8bbf8c734b2d8957dda62b621b4f5bd64302b6
Jasper Tey <jasper.tey@gmail.com>
2022-04-29 22:03:16 -0400

    Initials emails array in extractEmails()

--- src/Helpers/Text.php
+++ src/Helpers/Text.php
@@ -844,6 +844,8 @@ class Text
 
     public static function extractEmails($string)
     {
+        $emails = [];
+
         foreach (preg_split('/\s/', $string) as $token) {
             $email = filter_var(filter_var($token, FILTER_SANITIZE_EMAIL), FILTER_VALIDATE_EMAIL);
             if ($email !== false) {
