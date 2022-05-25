e5526361d04f9883d34b0a112f0efdcd47b20f8d
Jasper Tey <jasper.tey@gmail.com>
2022-05-25 15:46:38 -0400

    Settings Array implementation from io legacy.

---
+++ src/Legacy/io/extensions/data/SettingsArray.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Vio\Pinball\Legacy\io\extensions\data;
+
+use Vio\Pinball\Models\IoData;
+
+class SettingsArray
+{
+    static $_prefix = '';
+    protected static $_defaults = [];
+    protected $_cfg = [];
+    protected $_data = [];
+
+    public function __construct($prefix = '', $o = [])
+    {
+        if (is_array($prefix)) {
+            $o = $prefix;
+        } else {
+            $o['prefix'] = $prefix;
+        }
+
+        if (strlen(static::$_prefix)) {
+            $o['prefix'] = static::$_prefix;
+        }
+
+        $defaults = [
+            'obj' => null,
+            'oid' => null,
+            'prefix' => '',
+            'defaults' => static::defaults(),
+            'preload' => true,
+        ];
+        $o += $defaults;
+
+        $this->_cfg = $o;
+
+        if ($this->_cfg['preload']) {
+            $this->reload();
+        }
+    }
+
+    public static function defaults($params = null)
+    {
+        if (is_array($params)) {
+            static::$_defaults = $params;
+        }
+        return static::$_defaults;
+    }
+
+    public function setData($data)
+    {
+        $data = array_intersect_key($data, static::defaults());
+        $this->_data = $data;
+        return $this->_data;
+    }
+
+    protected function _key()
+    {
+        return static::$_prefix;
+    }
+
+    public function save()
+    {
+        if ($data = $this->data()) {
+            IoData::set($this->_key(), $data);
+        }
+        return true;
+    }
+
+    public function reload()
+    {
+        $prefix = addslashes($this->_cfg['prefix']);
+        $data = IoData::get($prefix) ?: [];
+        $this->setData($data);
+        return $this->data();
+    }
+
+    public function data()
+    {
+        $this->_data += static::defaults();
+        return $this->_data;
+    }
+
+    public function export()
+    {
+        return $this->data();
+    }
+
+    public static function get()
+    {
+        $settings = new static();
+        return $settings->export();
+    }
+}
